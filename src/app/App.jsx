c9cb90fda7786246a853400c26e03e27656df972
shawon <shawon.softeng@gmail.com>
2023-05-15 13:59:10 +0600

    pushing

---
+++ src/app/App.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import TextInput from "../components/ui/inputs/TextInput";
+import Button from "../components/ui/buttons/Button";
+import Text from "../components/ui/texts/Text";
+
+const App = () => {
+    return (
+        <div>
+            <h2> Hello world - APP </h2>
+            <TextInput />
+            <Button>test me</Button>
+            <Text line="lg" size="lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+                explicabo iusto? Rerum ex laborum doloribus, quam, in deserunt,
+                maxime saepe ipsa eveniet est laboriosam excepturi odit? Cum
+                voluptate modi sunt, quia dolorem nostrum officiis odit earum
+                rem vero nemo molestias fuga ex odio eaque dignissimos
+                cupiditate optio quibusdam suscipit quidem ullam corporis iusto
+                ad! Facere tempore ad praesentium atque aliquam a, obcaecati,
+                deleniti fugit sapiente ullam laborum error maiores unde
+                dignissimos quas at, enim quos quisquam nobis? Quia explicabo
+                nostrum dolorem. Earum tempore est soluta repellat voluptates
+                dolor consequatur explicabo odit adipisci pariatur quaerat nihil
+                aperiam excepturi ipsum doloremque, odio facilis itaque
+                necessitatibus sed incidunt animi ratione. Laboriosam quod, cum
+                aliquam unde nostrum animi fuga officia consectetur soluta nemo,
+                impedit tempore, sed incidunt saepe nobis! Repellendus
+                doloremque unde, illum beatae ipsa tempore atque praesentium,
+                recusandae repellat sed, quisquam facere. Reprehenderit, vero
+                eveniet repellat perferendis molestias quam eius dignissimos
+                odit modi, recusandae ea eos unde, sed quisquam cum laboriosam
+                assumenda! Perferendis maiores voluptatem in incidunt sequi,
+                nisi deserunt temporibus nihil quisquam dolores. Voluptatibus id
+                eaque voluptatem reiciendis assumenda at molestiae consectetur
+                sed est quaerat debitis nostrum, itaque quibusdam voluptas
+                facilis commodi enim cumque. Aperiam quis similique cumque autem
+                quos necessitatibus, est quasi laborum dolorum expedita,
+                deserunt voluptas possimus quibusdam totam nam ipsa doloremque,
+                delectus error impedit accusantium in debitis dicta! Ullam iste
+                illum provident molestiae repudiandae dolore quod dignissimos
+                aliquam a voluptate ut tenetur quas deleniti adipisci mollitia
+                earum asperiores ipsum molestias officia nemo accusamus, dolor
+                ipsa? Nemo est optio quidem nam enim, reiciendis sit eveniet
+                quisquam voluptatem maxime voluptas eum unde aliquid, quae sint
+                nulla expedita corporis cupiditate temporibus! Alias qui harum
+                provident architecto ab. Quibusdam similique fugiat voluptates
+                eligendi impedit ut reiciendis enim sit voluptatem corporis!
+                Repellendus ab magnam quas soluta rerum officia recusandae
+                reprehenderit cupiditate qui minus ea, quis ipsam vitae
+                exercitationem perspiciatis. Blanditiis, id est dolores ratione
+                nihil repellendus. Molestias eveniet quisquam voluptas atque
+                optio omnis harum eaque libero, rem sed accusamus voluptatum
+                dolore nam autem architecto neque! Aliquam iusto harum
+                voluptatum, quasi enim deleniti magni itaque possimus sapiente
+                omnis nam, incidunt, ipsam consequuntur odio maxime quam
+                voluptatibus in dolorem. Doloremque nulla possimus, recusandae
+                eaque, enim consequuntur maxime neque repellendus magnam
+                quisquam necessitatibus reiciendis quos, nam facilis repudiandae
+                voluptatem et beatae. Praesentium dicta error nesciunt,
+                assumenda ducimus molestias numquam eos porro pariatur
+                architecto cumque. Voluptatem error numquam maxime quisquam iure
+                quia. Molestiae qui ut deleniti porro modi id soluta vitae,
+                tempore laboriosam alias voluptates, deserunt, reprehenderit
+                ullam. Quia tempore aut pariatur odit, quam iusto laudantium
+                iure rerum laborum amet velit eveniet quibusdam in magni tempora
+                repudiandae suscipit accusantium dolores tenetur optio
+                consequuntur natus. Minima quaerat ducimus voluptate cumque
+                autem, laborum consequatur itaque, blanditiis alias animi
+                consectetur sed dolorem repudiandae ut dolor velit repellendus
+                fugiat porro illum quae voluptatem in perferendis. Deserunt
+                dolorem ullam, eum reiciendis sit commodi aliquid hic amet porro
+                sed, excepturi omnis? Reiciendis fuga iste, labore in ea
+                consectetur est doloremque ad soluta totam quis neque ullam,
+                obcaecati inventore quidem ab dolor placeat, iure suscipit.
+            </Text>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/ui/texts/Text.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const fontSizes = {
+    sm: "0.8rem",
+    md: "1rem",
+    lg: "1.2rem",
+};
+const lineHeights = {
+    sm: 1,
+    md: 1.2,
+    lg: 1.4,
+};
+const Text = styled.p`
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: ${(props) => fontSizes[props.size] ?? "1rem"};
+    color: #222;
+    line-height: ${(props) => lineHeights[props.line] ?? 1};
+`;
+export default Text;
